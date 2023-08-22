0024f3547815631df9ccd7344635b12640fa668f
Its-Yayo <a01754574@itesm.mx>
2023-08-22 10:00:00 -0600

    Fix channel enumeration #2

--- 1. Stenanography/extract_hidden_images.py
+++ 1. Stenanography/extract_hidden_images.py
@@ -10,13 +10,14 @@
 #           A01746999 Luis Eduardo Landeros Hernandez
 #----------------------------------------------------------
 
+import os
 import sys
-from PIL import *
+from PIL import Image
 
 def extract_hidden_images(input_file: str) -> None:
     """ Extracts hidden images from a file """
     try:
-        img = open(input_file)
+        img = Image.open(input_file)
     except Exception as e:
         print("File not found:", e)
         sys.exit(1)
@@ -25,18 +26,37 @@ def extract_hidden_images(input_file: str) -> None:
         print("Image is not RGB")
         sys.exit(1)
     
-    if not input_file.lower().endsWith('.png'):
+    if not input_file.lower().endswith('.png'):
         print("Image is not PNG")
         sys.exit(1)
     
-    width, height = img.size
     channels = img.split()
-    
 
+    for i, channel in enumerate(channels):
+        width, height = channel.size
 
+        channel_name = ["red", "green", "blue"][i]
+        base_filename, _ = os.path.splitext(input_file)
+        output_file = f"{base_filename}_channel_{i + 1}_{channel_name}.png"
+
+        hidden = Image.new('RGB', (width, height))
+
+        pixin = channel.load()
+        pixout = hidden.load()
+
+        for y in range(0, height):
+            for x in range(0, width):
+                pixout[x, y] = pixin[x, y] & 1
+
+        hidden.save(output_file)
 
 def main() -> None:
-    pass
+    if len(sys.argv) != 2:
+        print("Usage: python3 extract_hidden_images.py <input_file>")
+        sys.exit(1)
+    
+    input_file = sys.argv[1]
+    extract_hidden_images(input_file)
 
 if __name__ == '__main__':
     main()
