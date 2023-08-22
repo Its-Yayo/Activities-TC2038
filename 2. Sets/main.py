05dd9a6175cd0475eab4e1ff8096b6aefce66c78
Its-Yayo <a01754574@itesm.mx>
2023-08-22 10:45:50 -0600

    Update hash set #3

---
+++ 2. Sets/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import sys 
+from typing import Generic
+
+class OrderedSet(Generic[T]):
+    ...
+
+def main() -> None:
+    ordered_set = OrderedSet()
+    pass
+
+if __name__ == '__main__':
+    main()
+    sys.exit(1)
