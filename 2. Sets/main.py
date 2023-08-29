61f02103466ae7f30504ff1083ae54dfa0e8ca65
Its-Yayo <a01754574@itesm.mx>
2023-08-29 09:33:24 -0600

    Fix #2 Sets Main.py usage

--- 2. Sets/main.py
+++ 2. Sets/main.py
@@ -1,16 +1,39 @@
 #!/usr/bin/python3
 
-import sys 
+from __future__ import annotations
+import sys
 from typing import Generic, TypeVar
 
 T = TypeVar('T')
+N = TypeVar('N')
+
 
 class OrderedSet(Generic[T]):
-    ...
+    # FIXME: Order inherit values for __Node
+    class __Node(Generic[N]):
+        info: N | None
+        next: OrderedSet.__Node[N]
+        prev: OrderedSet.__Node[N]
+
+        def __init__(self, value: N | None) -> None:
+            self.info = value
+            self.next = self
+            self.prev = self
+
+    __sentinel: OrderedSet.__Node[T]
+
+    def __init__(self) -> None:
+        self.__sentinel = OrderedSet.__Node()
+
 
 def main() -> None:
-    ordered_set: OrderedSet[int] = OrderedSet()
-    pass
+    a: OrderedSet[int] = OrderedSet()
+    print(a)
+    print(f'{hash(a) = :x}')
+    print(f'{a is None = }')
+    print(f'{a == a = }')
+    print(f'{repr(a) = }')
+
 
 if __name__ == '__main__':
     main()
