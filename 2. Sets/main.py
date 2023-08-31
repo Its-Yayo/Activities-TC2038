1c7e0d9fffde78d44638d1c2124ce47694b6f5c5
yayo <88682615+Its-Yayo@users.noreply.github.com>
2023-08-30 19:14:23 -0600

    Update PEP8 Format

--- 2. Sets/main.py
+++ 2. Sets/main.py
@@ -9,6 +9,7 @@ N = TypeVar('N')
 
 
 class OrderedSet(Generic[T]):
+    
     # FIXME: Order inherit values for __Node
     class __Node(Generic[N]):
         info: N | None
@@ -22,6 +23,7 @@ class OrderedSet(Generic[T]):
 
     __sentinel: OrderedSet.__Node[T]
 
+    
     # Complexity -> O(N^2)
     def __init__(self, values=None) -> None:
         if values is None:
@@ -32,7 +34,7 @@ class OrderedSet(Generic[T]):
 
         for elem in values:
             self.add(elem)
-
+    
     # Complexity -> O(N)
     def add(self, value: T) -> None:
         if value in self:
@@ -44,7 +46,7 @@ class OrderedSet(Generic[T]):
         new_node.prev = self.__sentinel.prev
         self.__sentinel.prev.next = new_node
         self.__sentinel.prev = new_node
-
+    
     # Complexity -> O(N)
     def __repr__(self) -> str:
         result: list[T] = []
@@ -65,7 +67,7 @@ class OrderedSet(Generic[T]):
                 return True
             current = current.next
         return False
-
+    
     # Complexity -> O(N)
     def __len__(self) -> int:
         return self.__count
