7c7270b4d159ea0b3dd719a1367a0cbfa1cf0012
Its-Yayo <a01754574@itesm.mx>
2023-09-02 12:52:28 -0600

    Fix #11 Sets

---
+++ 2. Sets/sets.py
@@ -0,0 +1,41 @@
+from ordered_set import OrderedSet
+
+x = [3, 6, 10, 2]
+
+def iterations() -> None:
+    try:
+        it = iter(x)
+        print(f'{x = }, {it = }')
+        print(f'{next(it) = }')
+        print(f'{next(it) = }')
+        print(f'{next(it) = }')
+        print(f'{next(it) = }')
+        print(f'{next(it) = }')
+    except StopIteration:
+        print('stop')
+
+    try:
+        it = iter(x)
+        iterator = True
+
+        while iterator:
+        print(f'{next(it) = }')
+    except StopIteration:
+        print('stop')
+
+    for elem in x:
+        print(f'{elem = }')
+    else:
+        print('stop')
+
+def main() -> None:
+    iterations()
+
+    o = OrderedSet(['one', 'two', 'three', 'four', 'five'])
+    it2 = iter(o)
+    print(f'{next(it2) = }')
+    print(f'{next(it2) = }')
+
+    for str_elem in o:
+        print(f'{str_elem = }')
+
