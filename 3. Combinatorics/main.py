6c60929dbba55f2aadd41ce480e5c1e78b5adb88
Its-Yayo <a01754574@itesm.mx>
2023-09-08 10:43:52 -0600

    Fix #2 Combinatorics PEP8

--- 3. Combinatorics/main.py
+++ 3. Combinatorics/main.py
@@ -2,13 +2,17 @@
 
 from collections.abc import Iterable, Iterator
 import sys
+
+
 def example() -> Iterator[int]:
     for i in range(10):
         yield i
 
+
 def main() -> None:
     for i in example():
         print(i)
+
 
 if __name__ == '__main__':
     main()
