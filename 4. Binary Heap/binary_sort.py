d392d179978cc2efa234f80499cd722cd43523f0
Its-Yayo <a01754574@itesm.mx>
2023-09-26 10:45:36 -0600

    Add Usage #1 binary_sort.py.py

---
+++ 4. Binary Heap/binary_sort.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+from heapq import heappush, heappop
+import sys
+
+
+def heap_sort(data: list[int]) -> list[int]:
+    heap: list[int] = []
+
+    for elem in data:
+        heappush(heap, elem)
+
+    result: list[int] = []
+
+    while heap:
+        result.append(heappop(heap))
+
+    return result
+
+
+def main() -> None:
+    ...
+
+
+if __name__ == '__main__':
+    main()
+    sys.exit(1)
