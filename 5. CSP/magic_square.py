9dc257d1d8f6caae49dbd01fc963bc6c319fb442
Its-Yayo <a01754574@itesm.mx>
2023-09-29 10:16:27 -0600

    Add Usage #11 magic_square.py

--- 5. CSP/magic_square.py
+++ 5. CSP/magic_square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from abc import ABC
 from csp import Constraint, CSP
 from typing import NamedTuple, Optional, Dict, List, Tuple
 import sys
@@ -19,6 +20,18 @@ def check_square(grid: Grid) -> bool:
 
     return ((a + b + c) == (d + e + f) == (g + h + i)
             == (a + d + g) == (b + e + h) == (c + f + i))
+
+
+class MagicPurpleConstraint(Constraint[GridLocation, int], ABC):
+    def __init__(self, variables: list[int]):
+        super().__init__(variables)
+        self.variables: list[int] = variables
+
+    def satisfied(self, assignment: dict[GridLocation, int]) -> bool:
+        if len(assignment) == len(set(assignment.values())):
+            return False
+
+        return True
 
 
 def main() -> None:
