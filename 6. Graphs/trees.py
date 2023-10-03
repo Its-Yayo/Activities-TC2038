640604b4f9e4aaeb4d07b11a0ad2d62d5f46dd8a
Its-Yayo <a01754574@itesm.mx>
2023-10-03 09:36:52 -0600

    Add Usage #1 trees.py

---
+++ 6. Graphs/trees.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+from typing import Iterator, TypeVar, Generic, List, Optional
+from collections import deque
+
+Tree = Optional[list[any]]
+
+"""
+Tree:
+           a
+         /   \
+        b     c        
+       /       \      
+      d         e    
+               / \
+              f   g    
+        
+"""
+
+t: Tree = \
+    ['a',
+     ['b',
+      ['d', None, None],
+      None
+      ],
+     ['c',
+      None,
+      ['e',
+       ['f', None, None],
+       ['g', None, None]
+       ]
+      ]
+     ]
+
+
+# Inorder: d, b, a, c, f, e, g -> left, root, right
+# Preorder: a, b, d, c, e, f, g -> root, left, right
+# Postorder: d, b, f, g, e, c, a -> left, right, root
+
+def inorder(tree: Tree) -> Iterator[any]:
+    if tree is None:
+        return
+    yield from inorder(tree[1])
+    yield tree[0]
+    yield from inorder(tree[2])
+
+
+def preorder(tree: Tree) -> Iterator[any]:
+    if tree is None:
+        return
+    yield tree[0]
+    yield from preorder(tree[1])
+    yield from preorder(tree[2])
+
