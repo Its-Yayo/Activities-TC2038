a65df2b7a6d92fbe43587f9d4e0d4b30ce12d1b2
Its-Yayo <a01754574@itesm.mx>
2023-10-06 09:52:21 -0600

    Add Implementation Graphs #4

--- 6. Graphs/metrorrey/metrorrey.py
+++ 6. Graphs/metrorrey/metrorrey.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
 
+import sys
+from typing import Optional
+
+from graph import Graph
+from generic_search import bfs, node_to_path, Node
+
+
+def main() -> None:
+    metrorrey = Graph(["Talleres,"
+                       "San Bernabe",
+                       "Unidad Modelo"])
+
+    metrorrey.add_edge_by_vertices("Talleres", "San Bernabe")
+
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
