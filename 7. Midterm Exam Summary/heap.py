5d00519251f5af4d8e1a0cbf3228f18d252b328d
Its-Yayo <a01754574@itesm.mx>
2023-10-08 16:24:04 -0600

    Add Implementation Exam Overview #2

--- 7. Midterm Exam Summary/heap.py
+++ 7. Midterm Exam Summary/heap.py
@@ -1,12 +1,38 @@
 #!/usr/bin/python3
 
-import sys
+""" Instructions:
+Write in Python or pseudocode a function called heap_children.
+The function takes as input a list a containing a binary heap
+and an index i. It returns a tuple with the values of the left
+and right children corresponding to the element in the a[i].
+The tuple should contain None in place of the value of a
+non-existing child. Usage example:
 
-def heap_children() -> tuple[int]:
-    ...
+>>> heap: list[int] = [1, 3, 6, 5, 9, 8]
+>>> heap_children(heap, 1)
+(5, 9)
+>>> heap_children(heap, 2)
+(8, None) """
+
+
+import sys
+from typing import Optional, List, Tuple
+
+def heap_children(a: list[int], i: int) -> tuple[Optional[int], Optional[int]]:
+    left_index = (2 * i) + 1
+    right_index = (2 * i) + 2
+
+    left_nodes = (a[left_index] if left_index < len(a) else None)
+    right_nodes = (a[right_index] if right_index < len(a) else None)
+
+    return left_nodes, right_nodes
+
 
 def main() -> None:
-    ...
+    heap: list[int] = [1, 3, 6, 5, 9, 8]
+    print(heap_children(heap, 0))
+    print(heap_children(heap, 1))
+    print(heap_children(heap, 2))
 
 if __name__ == '__main__':
     main()
