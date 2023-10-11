15a5ff8177a73b86fa6acab3d88e7bab44a7c287
Its-Yayo <a01754574@itesm.mx>
2023-10-11 13:43:22 -0600

    Add Implementation Overview #3 puzzle FIXME

--- 5. CSP/cryptarithmetic_puzzle.py
+++ 5. CSP/cryptarithmetic_puzzle.py
@@ -21,11 +21,32 @@ from typing import Dict, List, Optional, Tuple
 
 
 def solve_cryptarithmetic_puzzle(addends: list[str], answer: str) -> Optional[dict[str, int]]:
-    ...
+
+    letters = set("".join(addends + [answer]))
+    letters = sorted(letters)
+
+    domains = {letter: list(range(10)) for letter in letters}
+
+    def valid_sum(assignment):
+        if len(set(assignment.values())) < len(letters):
+            return False
+
+        addend_values = [int("".join([str(assignment[letter]) for letter in addend])) for addend in addends]
+        answer_value = int("".join([str(assignment[letter]) for letter in answer]))
+
+        return sum(addend_values) == answer_value
+
+    problem = CSP(letters, domains)
+    problem.addConstraint(valid_sum, letters)
+
+    solution = problem.getSolution()
+
+    return solution
 
 
 def main() -> None:
-    ...
+    result = solve_cryptarithmetic_puzzle(['i', 'luv', 'u'], 'yes')
+    print(result)
 
 
 if __name__ == '__main__':
