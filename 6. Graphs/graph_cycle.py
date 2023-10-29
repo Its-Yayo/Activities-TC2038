f19af6dfef4fe4d18d97b9d82eabe667eaa46f62
Its-Yayo <a01754574@itesm.mx>
2023-10-29 15:07:12 -0600

    Add Implementation graph_cycle.py #3

--- 6. Graphs/graph_cycle.py
+++ 6. Graphs/graph_cycle.py
@@ -23,25 +23,6 @@ Graph = dict[str, list[str]]
 
 # TODO: Implement me!
 def has_cycle(initial: str, graph: Graph) -> Optional[list[str]]:
-    '''
-    visited: set[str] = set()
-    stack: list[str] = [initial]
-    parent: dict[str, str] = {initial: initial}
-
-    while stack:
-        current: str = stack.pop()
-
-        if current in visited:
-            return get_cycle(current, parent)
-
-        visited.add(current)
-
-        for neighbor in graph[current]:
-            if neighbor not in visited:
-                stack.append(neighbor)
-                parent[neighbor] = current
-
-    return None'''
     ...
 
 
