6a92b5d51b8a9a4d88b27dc05436a9773751d66d
Its-Yayo <a01754574@itesm.mx>
2023-10-31 10:33:10 -0600

    Add Implementation kruskal.py #2

--- 6. Graphs/kruskal.py
+++ 6. Graphs/kruskal.py
@@ -4,18 +4,38 @@ import sys
 from heapq import heapify, heappop
 from typing import Iterator, Optional, NamedTuple
 
+# FIXME: Fix object type
 WeightedGraph = dict[str, set(tuple(str, int))]
+
+
+class Edge(NamedTuple):
+    weight: int
+    u: str
+    v: str
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, Edge):
+            return (self.weight == other.weight
+                    and ((self.u == other.u and self.v == other.v)
+                    or (self.u == other.v and self.v == other.u)))
+        else:
+            return False
 
 
 def main() -> None:
     g1: WeightedGraph = {
-        'A': {('B', 4), ('C', 5)},
-        'B': {('A', 4)},
-        'C': {('A', 5), ('D', 6), ('E', 7)},
-        'D': {('C', 6), ('E', 2), ('F', 1)},
-        'E': {('C', 7), ('D', 2), ('F', 3)},
-        'F': {('D', 1), ('E', 3)}
+        'A': {(('B', 4), ('C', 5))},
+        'B': {(('A', 4))},
+        'C': {(('A', 5), ('D', 6), ('E', 7))},
+        'D': {(('C', 6), ('E', 2), ('F', 1))},
+        'E': {(('C', 7), ('D', 2), ('F', 3))},
+        'F': {(('D', 1), ('E', 3))}
     }
+
+    e1: Edge = Edge(4, 'A', 'B')
+    e2: Edge = Edge(4, 'A', 'B')
+
+    print(e1 == e2)
 
 
 if __name__ == '__main__':
