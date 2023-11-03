fa0fdf1fd3a942161a79a8a91333539a9063c06e
Its-Yayo <a01754574@itesm.mx>
2023-11-03 10:03:57 -0600

    test_2.py: add usages

--- Final Project/test_2.py
+++ Final Project/test_2.py
@@ -3,4 +3,10 @@
 import sys
 from dagor import JuegoCaballosBailadores, JugadorCaballosBailadoresAleatorio, JugadorCaballosBailadoresInteractivo
 
-...
+
+if __name__ == '__main__':
+    jugador1 = JugadorCaballosBailadoresInteractivo('Humano')
+    jugador2 = JugadorCaballosBailadoresAleatorio('Maquina')
+
+    juego = JuegoCaballosBailadores(jugador1, jugador2, 5, 6)
+    juego.inicia()
