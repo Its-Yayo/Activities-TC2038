43c34adde15839084c22e35a8037a51b39198b14
Its-Yayo <a01754574@itesm.mx>
2023-11-09 13:16:29 -0600

    changes: add resources

--- 6. Graphs/graph_search.py
+++ 6. Graphs/graph_search.py
@@ -71,7 +71,7 @@ def dfs_cycle(vertex: str, graph: Dict[str, List[str]], visited: set, parent_dic
             cycle.append(n)
             return cycle
 
-    return None
+    return None '''
 
 
 # Implementations for BFS Cycle detection
@@ -98,7 +98,7 @@ def bfs_cycle(vertex: str, parent: str, graph: Graph, visited: set) -> Iterator[
             if n != parent:
                 queue.append((n, current))
 
-    return None '''
+    return None
 
 def main() -> None:
     print(list(dfs('A', g)))
