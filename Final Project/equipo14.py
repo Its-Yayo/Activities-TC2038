35972c32f78c8b55fa6e8c4233b4826f3cbd4c4d
Its-Yayo <a01754574@itesm.mx>
2023-11-11 13:39:16 -0600

    equipo14.py: add usages

--- Final Project/equipo14.py
+++ Final Project/equipo14.py
@@ -29,18 +29,19 @@ class JugadorCaballosBailadoresEquipo14(JugadorCaballosBailadores):
     def heuristica(self, posicion):
         turno, _, _, rB, rN, cN, cB = posicion
         opponent_king = rN if turno == 'B' else rB
+        opponent_knight = cN if turno == 'B' else cB
         mine = cN if turno == 'N' else cB
 
         # Manhattan Distance for the other's king
         # If there's a place where the other's knight can kill mine's, it penalizes the move
         distance_king = abs(mine[0] - opponent_king[0]) + abs(mine[1] - opponent_king[1])
 
-        if self.triunfo(self._jugador_contrario(), posicion):
+        if self.triunfo(posicion):
             distance_king += 100
 
-        movements = len(self.posiciones_siguientes(posicion))
-        value = 5 * movements - 3 * distance_king 
-        
+        movements = self.posiciones_siguientes(posicion)
+        distance_knight_penalty = sum(1 for m in movements if isinstance(m[0], int) and isinstance(m[1], int) and abs(m[0] - opponent_knight[0]) + abs(m[1] - opponent_knight[1]) > 0)
+        value = 5 * len(movements) - 3 * distance_king - 2 * distance_knight_penalty
         return value
 
     def tira(self, posicion):
@@ -68,25 +69,27 @@ class JugadorCaballosBailadoresEquipo14(JugadorCaballosBailadores):
 
 
     # Minimax algorithm to make the best move
-    def minimax(self, tree, maximizing_player, alpha=float('-inf'), beta=float('inf')):
+    def minimax(self, tree, max):
         if len(tree) == 1:
             return tree[0]
 
-        best_value = float('-inf') if maximizing_player else float('inf')
-        best_move = None
+        if max:
+            max_value = float('-inf')
+            max_movement = None
 
-        for value, movement in tree:
-            if maximizing_player:
-                if value > best_value:
-                    best_value = value
-                    best_move = movement
-                alpha = max(alpha, best_value)
-            else:
-                if value < best_value:
-                    best_value = value
-                    best_move = movement
-                beta = min(beta, best_value)
+            for value, movement in tree:
+                if value > max_value:
+                    max_value = value
+                max_movement = movement
 
-        return best_value, best_move
+            return max_value, max_movement
+        else:
+            min_value = float('inf')
+            min_movement = None
 
+            for value, movement in tree:
+                if value > min_value:
+                    min_value = value
+                min_movement = movement
 
+            return min_value, min_movement
