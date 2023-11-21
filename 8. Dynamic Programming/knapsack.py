d55af5a6afdf42e4790b29c423c5499253ea28ba
Its-Yayo <a01754574@itesm.mx>
2023-11-21 09:41:19 -0600

    knapsack.py: update dataclass #2

--- 8. Dynamic Programming/knapsack.py
+++ 8. Dynamic Programming/knapsack.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from dataclasses import dataclass
+from pprint import pprint
 import sys
 
 @dataclass
@@ -18,8 +19,18 @@ class Entry:
 Table = list[list[Entry]]
 
 
+def solve(size: int, items: list[Item]) -> Table:
+    table: Table = [[Entry(0, [])] for _ in range(len(items))]
+
+
+
 def main() -> None:
-    ...
+    table: Table = solve(4, [Item('Guitar', 1, 1_500),
+                                        Item('Stereo', 4, 3_000),
+                                        Item('Laptop', 3, 2_000),
+                                        Item('IPhone', 1, 2_000)])
+
+    pprint(table)
 
 
 if __name__ == '__main__':
