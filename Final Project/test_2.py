22f92c05d0270d6f0544dc3adfd74b51f77b31a5
Its-Yayo <a01754574@itesm.mx>
2023-12-04 10:46:19 -0600

    equipo14.cpython-311-pyc: update stash

--- Final Project/test_2.py
+++ Final Project/test_2.py
@@ -12,5 +12,5 @@ if __name__ == '__main__':
     jugador4 = JugadorCaballosBailadoresInteractivo('Yo')
     jugador5 = JugadorCaballosBailadoresEquipo14('Equipo14')
 
-    juego = JuegoCaballosBailadores(jugador1, jugador2, 9, 5)
-    juego.inicia(veces=100, delta_max=2)
+    juego = JuegoCaballosBailadores(jugador1, jugador2, 7, 10)
+    juego.inicia(veces=1000, delta_max=2)
